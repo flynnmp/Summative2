0b38832fa8d1a6331f8207fa107ac6fd7cf907b3
flynnmp <62521077+flynnmp@users.noreply.github.com>
2020-04-02 18:18:51 +1300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ function toggleMenu() {
     //SET X TO BE OUR #menu ELEMENT
     var x = document.getElementById("dropmenu");
     //SET Y TO BE OUR #navButton ELEMENT
-    var y = document.getElementById("menubutton");
+    var y = document.getElementById("menuarrow");
 
     //TOGGLE (EITHER ADD OR REMOVE) THE .displayMenu CLASS TO #menu ELEMENT
     x.classList.toggle("displayMenu");
     //TOGGLE THE .fa-toggle-on CLASS FROM FONTAWESOME to the #navButton ELEMENT
-    y.classList.toggle("fa-toggle-on");
+    y.classList.toggle("fa-chevron-up");
 
 }
