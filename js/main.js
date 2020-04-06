73e446facd873cde957564b0076a408a7aaa8b23
flynnmp <62521077+flynnmp@users.noreply.github.com>
2020-04-06 15:32:53 +1200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,3 +14,19 @@ function toggleMenu() {
     y.classList.toggle("fa-chevron-up");
 
 }
+
+
+
+function expandContent() {
+
+    //SET X TO BE OUR #menu ELEMENT
+    var x = document.getElementById("content");
+    //SET Y TO BE OUR #navButton ELEMENT
+    var y = document.getElementById("menuplus");
+
+    //TOGGLE (EITHER ADD OR REMOVE) THE .displayMenu CLASS TO #menu ELEMENT
+    x.classList.toggle("expandContent");
+    //TOGGLE THE .fa-toggle-on CLASS FROM FONTAWESOME to the #navButton ELEMENT
+    y.classList.toggle("fa-chevron-up");
+
+}
